d07d74286c9fa6bc42a0fae780cf1a5a5da423f3
MarkBeef <s144039@student.dtu.dk>
2015-06-12 11:58:33 +0200

    Create BoxSettings.c

---
+++ BoxSettings.c
@@ -0,0 +1,32 @@
+#include <eZ8.h>
+#include <sio.h>
+#include "ansi.h"
+#include "mathfix.h"
+#include "charset.h"
+#include "GameSettings.h"
+
+struct box_t{ 
+	char x1,y1,x2,y2;
+	char length;
+	char width;
+	char pwrup;
+	char hp;
+};
+
+void setpwr(struct box_t *box_p) {
+	box_p -> pwrup = 0;
+
+} 
+
+void setBox(struct box_t *box_p) {
+		box_p ->length = 3;
+		box_p -> width = 1;
+		box_p -> hp = 2;
+		setpwr(box_p);
+		
+} 
+
+void changehp(struct box_t *box_p) {
+	box_p->hp--;
+	
+}
