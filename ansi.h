8f5450ce5198c0bcf32e7f4d6a056f9f1d73fc89
nickhpt <nickthanthrige@hotmail.dk>
2015-06-12 11:02:03 +0200

    Create ansi.h

---
+++ ansi.h
@@ -0,0 +1,17 @@
+#include <eZ8.h>             // special encore constants, macros and flash routines
+#include <sio.h>             // special encore serial i/o routine
+ 
+#define ESC 0x1B
+ 
+void fgcolor(int foreground);
+void bgcolor(int background);
+ 
+void color(int foreground, int background);
+ 
+void resetbgcolor();
+void clrscr();
+void gotoxy(int x, int y);
+void underline(char on);
+void reverse(char on);
+void blink(char on);
+void window(int x1, int y1, int x2, int y2, char* s, int style);
