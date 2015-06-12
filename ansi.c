fbff0df86b87c0710680ee6bba87125ab361ee4d
xXMagicPXx <s144022@student.dtu.dk>
2015-06-12 15:25:17 +0200

    Update ansi.c

--- ansi.c
+++ ansi.c
@@ -1,6 +1,7 @@
 #include <eZ8.h>             // special encore constants, macros and flash routines
 #include <sio.h>             // special encore serial i/o routine
-#include <string.h>
+#include <string.h>		        // For strlen() function
+#include "ansi.h"
  
 #define ESC 0x1B
  
@@ -58,69 +59,117 @@ void color(int foreground, int background) {
  
 void resetbgcolor() {
 // gray on black text, no underline, no blink, no reverse
-  printf("%c[m", ESC);  
+	printf("%c[m", ESC);  
 }
  
 void clrscr() { //clear screen
-  printf("%c[2J",ESC);
+	printf("%c[2J",ESC);
 }
  
  
 void clreol() { //clear to end of line
-  printf("%c[K",ESC);
+	printf("%c[K",ESC);
 }
  
-void gotoxy(int x, int y) { //Gå til givne koordinater
-  printf("%c[%d;%dH",ESC,x,y);
+void gotoxy(char x, char y) { //moves cursor to coordinates
+	printf("%c[%d;%dH",ESC,x,y);
 }
  
-void underline(char on) { //Understregning af ord
-  if(on != 0) {
-     printf("%c[04m",ESC);
-  } else {
-     printf("%c[24m",ESC);
-  }
+void underline(char on) { //underlines characters
+	  if(on != 0) {
+	     printf("%c[04m",ESC);
+	  } else {
+	     printf("%c[24m",ESC);
+	  }
 }
  
-void reverse(char on) { //Bytter om på forgrund og bagrunds - farven
-   if(on != 0) {
-     printf("%c[07m",ESC);
-   } else {
-     printf("%c[27m",ESC);
-   }
+void reverse(char on) { //Reverses foreground and background colors
+	   if(on != 0) {
+	     printf("%c[07m",ESC);
+	   } else {
+	     printf("%c[27m",ESC);
+	   }
 }
  
-void blink(char on) { //Teksten kommer til at blinke, når on forskellig fra 0
-  if(on != 0) {
-     printf("%c[05m",ESC);
-  } else {
-     printf("%c[25m",ESC);
-  }
+void blink(char on) { //Makes text blink
+	  if(on != 0) {
+	  	printf("%c[05m",ESC);
+	  } else {
+	  	printf("%c[25m",ESC);
+	  }
 }
  
-void window(int x1, int y1, int x2, int y2, char* s, int style) {
-  int i;
-  int n = strlen(s);
-  gotoxy(x1,y1);
-  color(0,15);
-  printf("%c%c",218,180);
-  reverse(1);
-  printf("%s",s);
-  for(i = (y1 + n + 2); i < y2 - 1; i++) {
-     printf(" ");
-  }
-  reverse(0);
-  printf("%c%c\n",195,191);
-  for(i = 1; i < x2; i++) {
-      gotoxy(x1 + i, y1);
-      printf("%c",179);
-      gotoxy(x1 + i, y2);
-      printf("%c",179);
-  }
-  gotoxy(x2,y1);
-  printf("%c",192);
-  for(i = y1; i < y2-1; i++) {
-      printf("%c",196);
-  }
-  printf("%c",217);
+void window(char x1, char y1, char x2, char y2, char* s, char style) {//Draws a window with (x1,y1) and (x2,y2) coordinates and writes the string s at the top
+	char i;
+	char n = strlen(s);
+	gotoxy(x1,y1);
+	color(0,15);
+	printf("%c%c",218,180);
+	reverse(1);
+	printf("%s",s);
+	for(i = (y1 + n + 2); i < y2 - 1; i++) {
+	   	printf(" ");
+	}
+	reverse(0);
+	printf("%c%c",195,191);
+	for(i = 1; i < x2; i++) {
+	   	gotoxy(x1 + i, y1);
+	   	printf("%c",179);
+	   	gotoxy(x1 + i, y2);
+	   	printf("%c",179);
+	}
+	gotoxy(x2,y1);
+	printf("%c",192);
+	for(i = y1; i < y2-1; i++) {
+	   	printf("%c",196);
+	}
+	printf("%c",217);
+}
+
+void drawsBoxX4(char x1,char y1,char x2, char y2) {
+	char i;
+	gotoxy(x1,y1);
+	printf("%c",218);
+	for(i = y1 + 1; i < y2; i++) {
+		printf("%c",196);
+	}
+	printf("%c",191);
+	for(i = 1; i =< x2; i++) {
+		gotoxy(x1 + i, y1);
+	   	printf("%c",179);
+	   	gotoxy(x1 + i, y2);
+	   	printf("%c",179);
+	}
+}
+
+void drawBox(char x1, char y1, char x2, char y2, char color) {
+	char i;
+	if(color == 5) 
+		fgcolor(4);
+    else if(color == 4)
+		fgcolor(2);
+	else if(color == 3)
+		fgcolor(10);
+	else if(color == 2)
+		fgcolor(11);
+	else if(color == 1)
+		fgcolor(1);
+	gotoxy(x1,y1);
+	printf("%c",218);
+	for(i = y1 + 1; i < length; i++) {
+		printf("%c",196);
+	}
+		printf("%c",191);
+	for(i = 1; i < x2; i++) {
+		gotoxy(x1 + i, y1);
+	   	printf("%c",179);
+	   	gotoxy(x1 + i, y2);
+		printf("%c",179);
+	}
+	gotoxy(x2,y1);
+	printf("%c",192);
+	for(i = y1; i < y2; i++) {
+		printf("%c",196);
+	}
+	printf("%c",217);
 }
