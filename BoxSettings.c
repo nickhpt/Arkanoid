98f3061e0efda41222d20b85903802fdea1d9f24
MarkBeef <s144039@student.dtu.dk>
2015-06-12 21:30:10 +0200

    Update BoxSettings.c

--- BoxSettings.c
+++ BoxSettings.c
@@ -2,9 +2,7 @@
 #include <sio.h>
 #include "ansi.h"
 #include "mathfix.h"
-#include "charset.h"
 #include "GameSettings.h"
-
 
 
 void setpwr(struct box_t *box_p) {
@@ -12,9 +10,11 @@ void setpwr(struct box_t *box_p) {
 
 } 
 
-void setBox(struct box_t *box_p, char hp1, char point) {
-		box_p ->length = 3;
-		box_p -> width = 1;
+void setBox(struct box_t *box_p, char xpos1, char ypos1,  char xpos2, char ypos2, char hp1, char point) {
+		box_p -> x1 = xpos1;
+		box_p -> y1 = ypos1; 
+		box_p -> x2 = xpos2;
+		box_p -> y2 = ypos2; 
 		box_p -> hp = hp1;
 		setpwr(box_p);
 		box_p -> boxpoint = point;
