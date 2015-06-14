59f0520670de4545db869f9e5e1c018596a0fbc3
xXMagicPXx <s144022@student.dtu.dk>
2015-06-14 13:50:28 +0200

    Update ansi.h

--- ansi.h
+++ ansi.h
@@ -14,7 +14,7 @@ void gotoxy(int x, int y);
 void underline(char on);
 void reverse(char on);
 void blink(char on);
-void window(int x1, int y1, int x2, int y2, char* s, int style);
+void window(char x1, char y1, char x2, char y2, char* s, char style);
 void drawsBoxX4(char x1,char y1,char x2, char y2);
 void drawBox(char x1, char y1, char x2, char y2, char color);
 void drawLine(char x1, char y1, char y2);
