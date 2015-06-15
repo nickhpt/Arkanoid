7d77185fdc1d40ac1cc9f97afcab46041866d439
MarkBeef <s144039@student.dtu.dk>
2015-06-15 10:19:36 +0200

    Create inputfix.h

---
+++ inputfix.h
@@ -0,0 +1,31 @@
+#include <eZ8.h>
+#include <sio.h>
+
+
+struct TTime {
+	char h, m, s, hs; 
+};
+
+#pragma interrupt
+void timer0int() ;
+
+//Initialiserer timeren
+void setTimer(); 
+
+//Initialiserer knapper
+char readKey(); 
+
+//Håndterer input, når timer skal bruges
+void inputHandler(char present, char next, char x, char y); 
+
+//Kan bruges til at finde rising edge
+char detectPush(char present, char next); 
+
+//Initialiserer display D1 (unit0), søjle 1 (den længste til venstre)
+void setUpLDS();
+
+//Viser ouput på den ene søjle på D1 displayet
+void flashCounter(unsigned char counter);
+
+
+
