3b4c50fb899c63ccb44ce0337fb1dc632ddd871f
MarkBeef <s144039@student.dtu.dk>
2015-06-16 16:15:05 +0200

    Update StrikerSettings.c

--- StrikerSettings.c
+++ StrikerSettings.c
@@ -6,7 +6,7 @@
 #include "inputfix.h"
 
 
-void setStriker(struct striker_t *striker_p, char xpos1, char xpos2, char ypos1) {
+void setStriker(struct striker_t *striker_p, unsigned char xpos1,unsigned char xpos2,unsigned char ypos1) {
 	striker_p -> ypos = ypos1;
 	striker_p -> points = 0;
 	striker_p -> lives = 3;
@@ -15,7 +15,7 @@ void setStriker(struct striker_t *striker_p, char xpos1, char xpos2, char ypos1)
 	printf("dfdfdf");
 }
 
-void initStriker(struct striker_t * striker_p, char xpos1, char xpos2, char ypos1) {
+void initStriker(struct striker_t * striker_p,unsigned char xpos1,unsigned char xpos2,unsigned char ypos1) {
 	striker_p -> ypos = ypos1;
 	striker_p -> points = 0;
 	striker_p -> lives = 3;
@@ -26,7 +26,7 @@ void initStriker(struct striker_t * striker_p, char xpos1, char xpos2, char ypos
 	striker_p -> rghtend = xpos2;
 }
 
-void change_striker(struct striker_t * striker_p, char xpos1, char xpos2) {
+void change_striker(struct striker_t * striker_p,unsigned char xpos1,unsigned char xpos2) {
 	striker_p -> lftend = xpos1;
 	striker_p -> rghtend = xpos2;
 	striker_p -> lftmid = striker_p -> lftend +1;
@@ -34,7 +34,7 @@ void change_striker(struct striker_t * striker_p, char xpos1, char xpos2) {
 	striker_p -> rghtmid = striker_p -> rghtend -1; 
 }
 
-void moveStriker(struct striker_t *striker_p, char direction) { 
+void moveStriker(struct striker_t *striker_p,unsigned char direction) { 
 	striker_p -> lftend += direction;
 	striker_p -> rghtend += direction;
 
@@ -44,15 +44,15 @@ void changelives(struct striker_t *striker_p) {
 	striker_p ->lives--;
 }
 
-void scorepoints(struct striker_t *striker_p, char point) {
+void scorepoints(struct striker_t *striker_p,unsigned char point) {
 	striker_p -> points += point;
 }
 
-void update_striker(struct striker_t * striker_p, char x1_wall, char x2_wall) {
-	char flag = returnflag();
+void update_striker(struct striker_t * striker_p,unsigned char x1_wall, unsigned char x2_wall) {
+	unsigned char flag = returnflag();
 	if(flag) {
-		char direction = 0;
-		char check = readKey();
+		unsigned char direction = 0;
+		unsigned char check = readKey();
 	
 		if(check == 4 && !(striker_p->lftend == x1_wall+1)) {
 			clearLine(striker_p-> ypos, striker_p->lftend, striker_p->rghtend);
