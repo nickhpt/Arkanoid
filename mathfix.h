d99386e1bb080df9e99a2f3c6631e20c84a8e5bf
xXMagicPXx <s144022@student.dtu.dk>
2015-06-17 00:19:40 +0200

    Update mathfix.h

--- mathfix.h
+++ mathfix.h
@@ -20,23 +20,23 @@ struct Ball {
 	   int angle;
 };
  
-void rotate(struct TVector *v, int n);
+void changeDirection(struct TVector *v, int n);
 void reflectBallRSidefromBelow(struct Ball *ball);
 void reflectBallRSidefromAbove(struct Ball *ball);
 void reflectBallLSidefromBelow(struct Ball *ball);
 void reflectBallLSidefromAbove(struct Ball *ball);
 void reflectBallTopfromRight(struct Ball *ball);
 void reflectBallTopfromLeft(struct Ball *ball);
+void reflectBallBottomfromRight(struct Ball *ball);
+void reflectBallBottomfromLeft(struct Ball *ball);
 
-
-
-void reflectBallSides(struct TVector *v);
-void reflectBallTop(struct TVector *v);
+void rotate(struct TVector *v, int n);
 long expand(long i);
 void printFix(long i);
 long sin(int n);
 long cos(int n);
-void progressBall(struct Ball *ball);
-void checkWallCollision(struct Ball* ball,char xmin, char ymin, char xmax, char ymax );
-void updateBall(struct Ball* ball,char xmin, char ymin, char xmax, char ymax );
 long round(long n);
+
+void progressBall(struct Ball *ball);
+void checkWallCollision(struct Ball* ball,unsigned char xmin,unsigned char ymin,unsigned char xmax,unsigned char ymax );
+void updateBall(struct Ball* ball,unsigned char xmin,unsigned char ymin,unsigned char xmax,unsigned char ymax);
