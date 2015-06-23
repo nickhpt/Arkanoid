8ceb94ffd2c96eeca2944277ec94273d842c4c12
xXMagicPXx <s144022@student.dtu.dk>
2015-06-23 16:19:33 +0200

    Mofos fucking

--- GameSettings.h
+++ GameSettings.h
@@ -2,6 +2,8 @@
 #include <sio.h>
 #include "ansi.h"
 
+
+
 struct box_t { 
 	unsigned char x1,y1,x2,y2;
 	unsigned char pwrup;
@@ -58,9 +60,15 @@ void reflectBallBottomTop(struct Ball *ball);
 void reflectBallStriker(struct Ball *ball, struct striker_t *striker);
 void boxImpact(struct Ball *ball, char *boxes, struct striker_t *striker, unsigned char oldx);
 
+void checkFailure(struct Ball *ball, struct striker_t *striker,unsigned char left, unsigned char right, unsigned char yposs);
 void progressBall(struct Ball *ball);
 void checkWallCollision(struct Ball* ball,unsigned char xmin,unsigned char ymin,unsigned char xmax,unsigned char ymax );
 void updateBall(struct Ball* ball, struct striker_t *striker,char *boxes, unsigned char xmin,unsigned char ymin,unsigned char xmax,
 				unsigned char ymax, unsigned char oldx);
 void make_boxes(char boxarray[14][5]);
 void initBall(struct Ball *ball);
+
+extern const char level1[14][5];
+extern const char level2[14][5];
+extern const char level3[14][5];
+
