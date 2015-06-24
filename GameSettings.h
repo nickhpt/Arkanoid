06295f188a1f11036912811afd39f6c2bbdab1ca
MarkBeef <s144039@student.dtu.dk>
2015-06-24 12:09:14 +0200

    Update GameSettings.h

--- GameSettings.h
+++ GameSettings.h
@@ -3,27 +3,13 @@
 #include "ansi.h"
 
 
-
-struct box_t { 
-	unsigned char x1,y1,x2,y2;
-	unsigned char pwrup;
-	unsigned char hp;
-	unsigned char boxpoint;
-};
-
-
 struct striker_t {
-	unsigned char lftend, rghtend;											//The different zones of the striker
-	unsigned char ypos;																	//leftend, leftmid, central, rightmid, rigthend
+	unsigned char lftend, rghtend;											
+	unsigned char ypos;																
 	int points;
 	unsigned char lives;										
 }; 
 
-void setpwr(struct box_t *box_p);
-
-void setBox(struct box_t *box_p,unsigned char xpos1,unsigned char ypos1,unsigned char xpos2,unsigned char ypos2,unsigned char hp1,unsigned char point);
-
-void changehp(struct box_t *box_p,unsigned char change);
 
 void setStriker(struct striker_t *striker_p,unsigned char xpos1,unsigned char xpos2,unsigned char ypos1);
 
@@ -31,9 +17,6 @@ void setStriker1(struct striker_t *striker_p, unsigned char xpos1,unsigned char 
 
 void moveStriker(struct striker_t *striker_p,unsigned char direction); 
 
-void changelives(struct striker_t *striker_p);
-
-void scorepoints(struct striker_t *striker_p,unsigned char point);
 
 void initStriker(struct striker_t * striker_p,unsigned char xpos1,unsigned char xpos2,unsigned char ypos1);
 
