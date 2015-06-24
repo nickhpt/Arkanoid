ffae938ce110696d9372d8116e97613ce0cc6429
MarkBeef <s144039@student.dtu.dk>
2015-06-24 12:09:46 +0200

    Update inputfix.h

--- inputfix.h
+++ inputfix.h
@@ -4,9 +4,6 @@
 
 
 
-struct TTime {
-	char h, m, s, hs; 
-};
 
 #pragma interrupt
 void timer0int();
@@ -20,14 +17,11 @@ char readKey();
 //Håndterer input, når timer skal bruges
 void inputHandler(char present, char next, char x, char y); 
 
-//Kan bruges til at finde rising edge
-char detectPush(char present, char next); 
 
 //Initialiserer display D1 (unit0), søjle 1 (den længste til venstre)
 void setUpLDS();
 
-//Viser ouput på den ene søjle på D1 displayet
-void flashCounter(unsigned char counter);
+
 
 void LEDinit();
 void LEDUpdate(char lives);
