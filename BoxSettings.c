32ef354625d1e5ad8d26595af216fd517bcd8b25
MarkBeef <s144039@student.dtu.dk>
2015-06-24 11:13:14 +0200

    Update BoxSettings.c

--- BoxSettings.c
+++ BoxSettings.c
@@ -54,27 +54,7 @@ const char level3[14][5] = {
 							{1,1,0,1,1}
 							};
 
-void setpwr(struct box_t *box_p) {
-	box_p -> pwrup = 0;
 
-} 
-
-void setBox(struct box_t *box_p,unsigned char xpos1,unsigned char ypos1,unsigned char xpos2,unsigned char ypos2,unsigned char hp1,unsigned char point) {
-		box_p -> x1 = xpos1;
-		box_p -> y1 = ypos1; 
-		box_p -> x2 = xpos2;
-		box_p -> y2 = ypos2; 
-		box_p -> hp = hp1;
-		setpwr(box_p);
-		box_p -> boxpoint = point;
-
-		
-} 
-
-void changehp(struct box_t *box_p,unsigned char change) {
-	box_p->hp += change;
-	
-}
 
 void make_boxes(char boxarray[14][5]) {
 	char i, j;
